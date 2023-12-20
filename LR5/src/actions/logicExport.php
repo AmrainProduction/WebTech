d0aba70b1533d0b2acfd15fe8d06ade0a3e77116
Amrain <atom7668@gmail.com>
2023-12-20 12:44:38 +0300

    last change LR5

--- LR5/src/actions/logicExport.php
+++ LR5/src/actions/logicExport.php
@@ -9,31 +9,47 @@ $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 if (isset($_POST['export']) && isset($_POST['path_to_save'])) {
 
-$pathToSave = $_POST['path_to_save'];
+    $pathToSave = $_POST['path_to_save'];
 
-// Защита от перехода в родительскую директорию
-$pathToSave = ltrim($pathToSave, '/');
+    // Защита от перехода в родительскую директорию
+    $pathToSave = ltrim($pathToSave, '/');
 
-// Проверка наличия символов ".."
-if (str_contains($pathToSave, '..')) {
-die('Ошибка: Попытка навигации в родительскую директорию.');
-}
+    // Проверка наличия символов ".."
+    if (str_contains($pathToSave, '..')) {
+        die('Ошибка: Попытка навигации в родительскую директорию.');
+    }
 
-// Путь для сохранения файла JSON
-$filePath = $_SERVER['DOCUMENT_ROOT'] . '/Sites/LR5/' . $pathToSave;
+    // Путь для сохранения файла JSON
+    $filePath = $_SERVER['DOCUMENT_ROOT'] . '/Sites/LR5/' . $pathToSave;
 
-// Создание файла CSV и запись данных в него
-$csvFile = fopen("$pathToSave", 'w');
-fprintf($csvFile, chr(0xEF) . chr(0xBB) . chr(0xBF)); // Эта строка добавляет в начало файла CSV последовательность байтов (0xEF, 0xBB, 0xBF), которая является сигнатурой кодировки UTF-8 с маркером порядка байтов (BOM). BOM (Byte Order Mark) используется для указания порядка байтов в текстовом файле и помогает правильно интерпретировать файл в кодировке UTF-8.
-foreach ($data as $row) {
-fputcsv($csvFile, $row);
-}
-fclose($csvFile);
+    // Создание файла CSV и запись данных в него
+    $csvFile = fopen("$pathToSave", 'w');
+    fprintf($csvFile, chr(0xEF) . chr(0xBB) . chr(0xBF)); // Эта строка добавляет в начало файла CSV последовательность байтов (0xEF, 0xBB, 0xBF), которая является сигнатурой кодировки UTF-8 с маркером порядка байтов (BOM). BOM (Byte Order Mark) используется для указания порядка байтов в текстовом файле и помогает правильно интерпретировать файл в кодировке UTF-8.
+    foreach ($data as $row) {
+        fputcsv($csvFile, $row);
+    }
+    fclose($csvFile);
 
-$uploadUrl = '/Sites/LR5/worker.php?file=' . urlencode($pathToSave);
+    // Send the CSV file to worker.php
 
-return $resExport = "<a href='$uploadUrl' download='$pathToSave' class='color-link-ex'>Скачать файл</a>";
+    $url = 'http://localhost/Sites/LR5/worker.php';
+    $curl = curl_init($url);
+
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, [
+        'file' => new CURLFile($filePath, 'application/vnd.ms-excel', $pathToSave)
+    ]);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+    $response = curl_exec($curl);
+    print_r($response);
+    
+    if ($response === false) {
+        throw new Exception("Ошибка отправки файла скрипту worker.php");
+    }
+
+    curl_close($curl);
 } else {
-return $resExport = '';
+    return $resExport = '';
 }
 ?>
--- LR5/worker.php
+++ LR5/worker.php
@@ -5,8 +5,8 @@ $uploadDir = $_SERVER['DOCUMENT_ROOT'] . '/Sites/LR5/';
 $types = [
     'application/vnd.ms-excel',
 ];
-//var_dump($_FILES);
-if ($_SERVER["REQUEST_METHOD"] == "GET" && $_FILES) {
+var_dump($_SERVER["REQUEST_METHOD"], $_FILES);
+if ($_SERVER["REQUEST_METHOD"] == "POST" && $_FILES) {
 
     if (!file_exists($uploadDir)) {
         mkdir($uploadDir);
@@ -15,7 +15,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && $_FILES) {
     $file = array_shift($_FILES);
     if (in_array($file['type'], $types)) {
         if (move_uploaded_file($file['tmp_name'], $uploadDir . $file['name'])) {
-            echo "<a href='/Sites/LR5/{$file['name']}' download='{$file['name']}'>Ссылка на скачивание файла</a>";
+            echo "<a href='/Sites/LR5/{$file['name']}' download='{$file['name']}' class='color-link-ex'>Ссылка на скачивание файла</a>";
         } else {
             echo 'Файл не был загружен';
         }
